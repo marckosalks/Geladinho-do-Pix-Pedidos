4e10461a7c2fe5784fe73d0d70c32652afb5f0c8
marckosalks <m4rcos.sales@gmail.com>
2025-03-07 23:57:44 -0300

    hot: arrumando possivel bug

--- src/components/ListaSabores/ListaSabores.tsx
+++ src/components/ListaSabores/ListaSabores.tsx
@@ -2,15 +2,15 @@ import { Images } from "../../assets"
 import "./styles.css"
 
 export function SaborLista(){
-  const sabores: string[] =  ["Coco", "Morango", "Manga", "Maracuja"]
+  const sabores = ["Coco", "Morango", "Manga", "Maracuja"]
 
   return(
     <section className="saboresContainer">
         <ul>
         {sabores.map((sabor: string, index ) => (
-           <li key={index}>
+           <li key={sabor}>
             <img src={Images[index]} alt="" className="geladinhos"/>
-            <a href="">{sabor}</a>
+            <a href="" >{sabor}</a>
             </li>
         ))}
         </ul>
